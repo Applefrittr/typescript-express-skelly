d484106258391b1b279f8074e5c67e39df7d5d13
applefrittr <jacob.lee.jordan@gmail.com>
2024-11-01 21:38:55 -0400

    error listener added to index.ts, update package.json

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
 import app from "./app";
 import http from "http";
-const debug = require("debug")("api:server");
+const debug = require("debug")("typescript-express-skelly:server");
 
 const server = http.createServer(app);
 
 server.listen(process.env.PORT || 6969);
 server.on("listening", onListening);
+server.on("error", (err) => {
+  debug(err.message);
+});
 
 function onListening() {
   const addr = server.address();
